f1842c7e25fee685e7da690934befc5001fbe1c6
temote <i.temote315@gmail.com>
2021-02-11 17:32:29 +0900

    Create Age feature as a feather format

--- features/funcs.py
+++ features/funcs.py
@@ -1,10 +1,20 @@
 import pandas as pd
 
 
-def load_feather(df1, file_name, col_name):
-    df2 = pd.read_feather(file_name)
-    df2 = df2[['PassengerId', col_name]]
-    if col_name in df1.columns:
-        df1 = df1.drop(col_name, axis=1)
-    df = pd.merge(df1, df2, on='PassengerId', how='inner')
+def load_feather(df1, col_dict):
+    for col_name in col_dict.keys():
+        df2 = pd.read_feather(col_dict[col_name])
+        df2 = df2[['PassengerId', col_name]]
+        if col_name in df1.columns:
+            df1 = df1.drop(col_name, axis=1)
+        df = pd.merge(df1, df2, on='PassengerId', how='inner')
     return df
+
+
+# def load_feather(df1, file_name, col_name):
+#     df2 = pd.read_feather(file_name)
+#     df2 = df2[['PassengerId', col_name]]
+#     if col_name in df1.columns:
+#         df1 = df1.drop(col_name, axis=1)
+#     df = pd.merge(df1, df2, on='PassengerId', how='inner')
+#     return df
