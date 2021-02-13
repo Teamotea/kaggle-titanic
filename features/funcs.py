dee7e27a80cddc7e4a65ecb0fd3104bc2115a7b8
temote <i.temote315@gmail.com>
2021-02-13 16:36:40 +0900

    Create one-hot-encoded features

--- features/funcs.py
+++ features/funcs.py
@@ -7,14 +7,11 @@ def load_feather(df1, col_dict):
         df2 = df2[['PassengerId', col_name]]
         if col_name in df1.columns:
             df1 = df1.drop(col_name, axis=1)
-        df = pd.merge(df1, df2, on='PassengerId', how='inner')
+        df1 = pd.merge(df1, df2, on='PassengerId', how='inner')
+    return df1
+
+
+def load_feather_ohe(df1, file_path):
+    df2 = pd.read_feather(file_path)
+    df = pd.merge(df1, df2, on='PassengerId', how='inner')
     return df
-
-
-# def load_feather(df1, file_name, col_name):
-#     df2 = pd.read_feather(file_name)
-#     df2 = df2[['PassengerId', col_name]]
-#     if col_name in df1.columns:
-#         df1 = df1.drop(col_name, axis=1)
-#     df = pd.merge(df1, df2, on='PassengerId', how='inner')
-#     return df
